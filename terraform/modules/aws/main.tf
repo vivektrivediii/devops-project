b1f07bc3ac76c9c260e200b1468ff54397d9316a
vivektrivediii <harshil@neuroverse.build>
2025-04-06 01:29:03 +0530

    test

--- terraform/modules/aws/main.tf
+++ terraform/modules/aws/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  vpc_security_group_ids =  [aws_security_group.sg.id] 
+  # vpc_security_group_ids =  [aws_security_group.sg.id] 
 
   tags = {
     Name = "PaperSocialAWS"
