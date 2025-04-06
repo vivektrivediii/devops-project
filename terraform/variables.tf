11e88ab31154e78c077bcb058e5bf1565e764e66
vivektrivediii <harshil@neuroverse.build>
2025-04-06 14:05:23 +0530

    az

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,10 +17,19 @@ variable "sg_description" {
 variable "resource_group_name" {}
 variable "location" {}
 variable "vm_name" {}
-variable "admin_username" {}
-variable "admin_password" {
-  sensitive = true
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default = "azureuser"
 }
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+  default = "P@ssword1234!"
+}
+
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {
