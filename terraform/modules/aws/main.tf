46cf6e1a4ac60639552896a25b48a38883830737
Vivek Trivedi <harshil@neuroverse.build>
2025-04-06 16:18:12 +0530

    az

--- terraform/modules/aws/main.tf
+++ terraform/modules/aws/main.tf
@@ -26,6 +26,12 @@ resource "aws_security_group" "this" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }  
+    ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
 
   egress {
     from_port   = 0
--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -101,6 +101,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "Allow-5000"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
