fdc60ca8c8f9543ec3d47edb99c21fdc2bc5ad22
vivektrivediii <harshil@neuroverse.build>
2025-04-06 14:35:02 +0530

    az

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,13 +14,16 @@ module "ec2_instance" {
 
 
 #### az #######
+# provider "azurerm" {
+#   features {}
+#   subscription_id = var.subscription_id
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
+#   tenant_id       = var.tenant_id
+#   use_cli = false
+# }
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  use_cli = false
 }
 
 module "vm" {
