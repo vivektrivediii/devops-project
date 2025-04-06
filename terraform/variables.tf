2a68de89bbb9a0b6e39ef634631fb0bd5a8666c1
Vivek Trivedi <vtrivedi6899@gmail.com>
2025-04-06 13:21:00 +0530

    az

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,52 @@ variable "sg_description" {
   default = "Allow HTTP and SSH"
 }
 
+
+####az###
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name of the Virtual Machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure client secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
+    
