d5b3eabf7cfd26c01e8e1cd492c579be4f44e113
Prince2903 <prince.patel@prama.ai>
2025-04-06 13:31:58 +0530

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,51 +15,51 @@ variable "sg_description" {
 
 ####az###
 variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
+  # description = "Name of the resource group"
+  # type        = string
 }
 
 variable "location" {
-  description = "Azure region for resources"
-  type        = string
+  # description = "Azure region for resources"
+  # type        = string
 }
 
 variable "vm_name" {
-  description = "Name of the Virtual Machine"
-  type        = string
+  # description = "Name of the Virtual Machine"
+  # type        = string
 }
 
 variable "admin_username" {
-  description = "Admin username for the VM"
-  type        = string
+  # description = "Admin username for the VM"
+  # type        = string
 }
 
 variable "admin_password" {
-  description = "Admin password for the VM"
-  type        = string
-  sensitive   = true
-  default = "P@ssword1234!"
+  # description = "Admin password for the VM"
+  # type        = string
+  # sensitive   = true
+  # default = "P@ssword1234!"
 }
 
 
 variable "subscription_id" {
-  type        = string
-  description = "Azure subscription ID"
+  # type        = string
+  # description = "Azure subscription ID"
 }
 
 variable "client_id" {
-  type        = string
-  description = "Azure client ID"
+  # type        = string
+  # description = "Azure client ID"
 }
 
 variable "client_secret" {
-  type        = string
-  description = "Azure client secret"
-  sensitive   = true
+  # type        = string
+  # description = "Azure client secret"
+  # sensitive   = true
 }
 
 variable "tenant_id" {
-  type        = string
-  description = "Azure tenant ID"
+  # type        = string
+  # description = "Azure tenant ID"
 }
     
