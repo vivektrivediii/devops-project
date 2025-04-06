4be863680df89c3eb9f7e3666efec126b800d8f1
vivektrivediii <harshil@neuroverse.build>
2025-04-06 11:30:48 +0530

    test

--- terraform/generate_inventory.py
+++ terraform/generate_inventory.py
@@ -8,5 +8,5 @@ data = json.loads(output)
 instance_ip = data["aws_instance_ip"]["value"]
 
 # Write to inventory file
-with open("ansible/inventory_aws.ini", "w") as f:
+with open("./../ansible/inventory_aws.ini", "w") as f:
     f.write(f"[web]\n{instance_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/devops.pem\n")
