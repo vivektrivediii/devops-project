af7a0d6b8e9eba994522455cc8cdd85d33fc317c
Vivek Trivedi <vtrivedi6899@gmail.com>
2025-04-06 13:15:21 +0530

    az

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "ec2_instance" {
-  source          = "./modules/aws"
-  ami_id          = var.ami_id
-  instance_type   = var.instance_type
-  key_name        = var.key_name
-  instance_name   = var.instance_name
-  sg_name         = var.sg_name
-  sg_description  = var.sg_description
-}
+# module "ec2_instance" {
+#   source          = "./modules/aws"
+#   ami_id          = var.ami_id
+#   instance_type   = var.instance_type
+#   key_name        = var.key_name
+#   instance_name   = var.instance_name
+#   sg_name         = var.sg_name
+#   sg_description  = var.sg_description
+# }
 
 
 #### az #######
