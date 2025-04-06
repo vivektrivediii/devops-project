6b2d5cc98466716d4ce6558ecc1cf6de5571470b
vivektrivediii <harshil@neuroverse.build>
2025-04-06 14:01:40 +0530

    az

--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -4,7 +4,13 @@ variable "location" {}
 
 variable "vm_name" {}
 
-variable "admin_username" {}
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
 
-
-variable "admin_password" {}
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+}
