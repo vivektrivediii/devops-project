5608aa845428e97d5ab997df2ef13d23bf504071
vivektrivediii <harshil@neuroverse.build>
2025-04-06 12:18:37 +0530

    test

--- terraform/modules/aws/main.tf
+++ terraform/modules/aws/main.tf
@@ -20,6 +20,12 @@ resource "aws_security_group" "this" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
 
   egress {
     from_port   = 0
