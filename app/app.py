b09dedc46ba4fcc396b8772384dd08b8612ec34c
vivektrivediii <harshil@neuroverse.build>
2025-04-06 12:25:10 +0530

    test

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Vivek!"
+    return "Hello from VivekT!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
